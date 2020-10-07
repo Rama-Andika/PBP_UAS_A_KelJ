eb4f55bbb3a71dd7005855c3da48d17c737e49cc
Rama-Andika <ramaandika31@gmail.com>
2020-10-07 11:02:00 +0800

    master file

--- app/src/main/java/com/example/tubes/MainActivity.java
+++ app/src/main/java/com/example/tubes/MainActivity.java
@@ -6,24 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    private MaterialButton btn_login;
+    private TextView link_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MaterialButton btn_login = findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        link_signup = (TextView) findViewById(R.id.link_signup);
+        link_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, Signup.class);
                 startActivity(i);
             }
         });
+        MaterialButton btn_login = findViewById(R.id.btn_login);
+
     }
 }
