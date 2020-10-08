14f222ea1ba53bec9cec95dea398824a75172c77
Rama-Andika <ramaandika31@gmail.com>
2020-10-08 20:24:27 +0800

    master file

--- app/src/main/java/com/example/tubes/LoginActivity.java
+++ app/src/main/java/com/example/tubes/LoginActivity.java
@@ -1,34 +1,160 @@
 package com.example.tubes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+    private TextInputEditText input_email;
+    private TextInputEditText input_password;
+    private MaterialButton btn_login;
     private TextView link_signup;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        input_email = (TextInputEditText) findViewById(R.id.input_emailR);
+        input_password = (TextInputEditText) findViewById(R.id.input_passwordR);
+        btn_login = (MaterialButton) findViewById(R.id.btn_login);
         link_signup = (TextView) findViewById(R.id.link_signup);
-        link_signup.setOnClickListener(new View.OnClickListener() {
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(LoginActivity.this, Signup.class);
-                startActivity(i);
+                login();
             }
         });
-        MaterialButton btn_login = findViewById(R.id.btn_login);
 
+        link_signup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        btn_login.setEnabled(false);
+
+
+
+        String input1 = input_email.getText().toString();
+        String input2 = input_password.getText().toString();
+        mFirebaseAuth.signInWithEmailAndPassword(input1,input2).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "SignIn Unccessfull, Please Try Again", Toast.LENGTH_SHORT).show();
+                    btn_login.setEnabled(true);
+                }else{
+                    final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme_Dark_Dialog);
+                    progressDialog.setIndeterminate(true);
+                    progressDialog.setMessage("Authenticating...");
+                    progressDialog.show();
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    // On complete call either onLoginSuccess or onLoginFailed
+                                    onLoginSuccess();
+                                    // onLoginFailed();
+                                    progressDialog.dismiss();
+                                }
+                            }, 3000);
+                }
+            }
+        });
+
+
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        btn_login.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        btn_login.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = input_email.getText().toString();
+        String password = input_password.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            input_email.setError("enter a valid email address");
+            valid = false;
+        } else if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            input_password.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            input_password.setError(null);
+            input_email.setError(null);
+
+        }
+
+        return valid;
     }
 }
---
+++ app/src/main/java/com/example/tubes/SignupActivity.java
@@ -0,0 +1,182 @@
+package com.example.tubes;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    TextInputEditText input_username;
+    TextInputEditText input_name;
+    TextInputEditText input_emailR;
+    TextInputEditText input_number;
+    TextInputEditText input_passwordR;
+    MaterialButton btn_signup;
+    TextView link_login;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        input_name = (TextInputEditText) findViewById(R.id.input_name);
+        input_emailR = (TextInputEditText) findViewById(R.id.input_emailR);
+        input_number = (TextInputEditText) findViewById(R.id.input_number);
+        input_username = (TextInputEditText) findViewById(R.id.input_username);
+        input_passwordR = (TextInputEditText) findViewById(R.id.input_passwordR);
+        btn_signup = (MaterialButton) findViewById(R.id.btn_signup);
+        link_login = (TextView) findViewById(R.id.link_login);
+
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        link_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+
+
+
+
+
+        String name = input_name.getText().toString();
+        String input1 = input_emailR.getText().toString();
+        String mobile = input_number.getText().toString();
+        String user = input_username.getText().toString();
+        String input2 = input_passwordR.getText().toString();
+
+        mFirebaseAuth.createUserWithEmailAndPassword(input1,input2).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "SignUp Unccessfull, Please Try Again", Toast.LENGTH_SHORT).show();
+                    btn_signup.setEnabled(true);
+                }
+                else{
+                    final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this, R.style.AppTheme_Dark_Dialog);
+                    progressDialog.setIndeterminate(true);
+                    progressDialog.setMessage("Creating Account...");
+                    progressDialog.show();
+
+                    new Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    // On complete call either onSignupSuccess or onSignupFailed
+                                    // depending on success
+                                    onSignupSuccess();
+                                    // onSignupFailed();
+                                    progressDialog.dismiss();
+                                }
+                            }, 3000);
+                }
+
+            }
+        });
+
+
+
+    }
+
+
+    public void onSignupSuccess() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        btn_signup.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = input_name.getText().toString();
+        String email = input_emailR.getText().toString();
+        String mobile = input_number.getText().toString();
+        String user = input_username.getText().toString();
+        String password = input_passwordR.getText().toString();
+
+
+        if (name.isEmpty() || name.length() < 3) {
+            input_name.setError("at least 3 characters");
+            valid = false;
+        } else {
+            input_name.setError(null);
+        }
+
+        if (user.isEmpty()) {
+            input_username.setError("Enter Valid Username");
+            valid = false;
+        } else {
+            input_username.setError(null);
+        }
+
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            input_emailR.setError("enter a valid email address");
+            valid = false;
+        } else {
+            input_emailR.setError(null);
+        }
+
+        if (mobile.isEmpty() || mobile.length()!=10) {
+            input_number.setError("Enter Valid Mobile Number");
+            valid = false;
+        } else {
+            input_number.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            input_passwordR.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            input_passwordR.setError(null);
+        }
+
+
+        return valid;
+    }
+}
