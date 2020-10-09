e54741288ee710335aea7f1c79aa5bccceb20317
Rama-Andika <ramaandika31@gmail.com>
2020-10-09 11:58:59 +0800

    master file

--- app/src/main/java/com/example/tubes/HomeActivity.java
+++ app/src/main/java/com/example/tubes/HomeActivity.java
@@ -2,15 +2,38 @@ package com.example.tubes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    MaterialButton btn_signout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_home);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signOut(View view) {
+        mFirebaseAuth.signOut();
+        finish();
+        Intent i = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    public void profileMenu(View view) {
+        Intent i = new Intent(HomeActivity.this, ProfileActivity.class);
+        startActivity(i);
     }
 }
